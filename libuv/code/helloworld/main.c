408dfb72096411816f08ccf09dabb7eb466419d5
小昆哥 <969352531@qq.com>
2017-01-16 16:08:37 +0800

    Signed-off-by: 小昆哥 <969352531@qq.com> timer测试

--- libuv/code/helloworld/main.c
+++ libuv/code/helloworld/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <uv.h>
 
+static void  test(struct uv_timer_s *time, int aa)
+{
+	static int num = 0;
+	printf("\n\n\nhello[%d]\n\n\n",num++);
+}
+
 int main() {
+#if 0
     uv_loop_t *loop = uv_loop_new();
 
     printf("Now quitting.\n");
     uv_run(loop, UV_RUN_DEFAULT);
-
+#endif
+	uv_timer_t ti;
+	uv_timer_init(uv_default_loop(),&ti);
+	uv_timer_start(&ti,test,1000,1000);
+	uv_run(uv_default_loop(),UV_RUN_DEFAULT);
     return 0;
 }
