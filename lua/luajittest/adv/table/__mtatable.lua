9eb3583a693ad0d27d05ac6a5f21d538951c5aab
unknown <mukunkong@zwt-dc03.corp.qihoo.net>
2018-03-02 17:32:58 +0800

    metatable shuxing __metatable

---
+++ lua/luajittest/adv/table/__mtatable.lua
@@ -0,0 +1,3 @@
+object = setmetatable({},{__metatable = "You cannot access here",name="akon"})
+print(getmetatable(object)) --> You cannot access here
+setmetatable(object, {})    --> в§ЗЂБрвыЦїБЈДэ
